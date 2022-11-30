0f3468fb0cc0ce85dc5a4096652387e2d003e6f1
LTstrange <835422774@qq.com>
2022-11-30 19:22:42 +0800

    Write a BNF class, to store grammar definitions (in text).

--- analyzer.py
+++ analyzer.py
@@ -24,6 +24,10 @@ class Analyzer:
 
 
 class Lexer:
+    """
+    Lexer
+    """
+
     def __init__(self):
         self._terminals = dict()
 
@@ -39,8 +43,37 @@ class Lexer:
 
 
 class BNF:
+    """
+    Grammar
+    """
+
     def __init__(self):
-        ...
-    
-    def set_from_text(self, text):
-        print(text)
+        self._definitions = dict()
+
+    def set_from_text(self, text: str):
+        # FIRST: get each definition
+        # merge multi line def into one line
+
+        lines = [line.strip() for line in text.split("\n") if line.strip()]
+        ID = ""
+        for line in lines:
+            result = re.match(ID_pattern, line)
+            if result:
+                # means this is a new definition
+                ID = result.group()
+                line = line[result.span()[1]:].lstrip()
+                if not line.startswith("::="):
+                    raise Exception("BNF grammar Error!!!: Missing \"::=\"")
+
+                body = line.lstrip("::=")
+                self._definitions[ID] = body.strip()
+            else:
+                # means this line need to attach to previous def
+                self._definitions[ID] += " " + line.strip()
+
+        self.show_definitions()
+
+    def show_definitions(self):
+        for key, value in self._definitions.items():
+            print(key, end="\t->\t")
+            print(value)
