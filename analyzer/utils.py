cac38f0816847faf12ea0c7c78c5dda17e256f22
LTstrange <835422774@qq.com>
2022-12-06 16:40:23 +0800

    Modify eat_token_by_token to match_token_by_token. And add more comments.

--- analyzer/utils.py
+++ analyzer/utils.py
@@ -4,13 +4,13 @@
 import re
 
 
-def eat_token_by_token(text: str, regexes: [str], symbols: [str], exclude=None) -> [(int, str)]:
-    if exclude is None:
+def match_token_by_token(text: str, regexes: [str], letters: [str], exclude=None) -> [(int, str)]:
+    if exclude is None:  # default exclude set
         exclude = [r"[\s]+"]
     tokens: [(int, str)] = []
     ind = 0
     while ind < len(text):
-        content = text[ind:]
+        content = text[ind:]  # For the remaining characters in text
         matches = []
         # FIRST: exclude
         has_exclude = False
@@ -22,17 +22,20 @@ def eat_token_by_token(text: str, regexes: [str], symbols: [str], exclude=None) 
         if has_exclude:
             continue
 
-        # SECOND: match regex
+        # SECOND: regex match
         for i, pat in enumerate(regexes):
             if out := re.match(pat, content):
                 matches.append((i, out.group()))  # matched string and string's length
         reg_len = len(regexes)
 
-        # THIRD: match symbol
-        for i, sym in enumerate(symbols):
+        # THIRD: direct match
+        for i, sym in enumerate(letters):
             if content.startswith(sym):
                 matches.append((i + reg_len, sym))
+        # todo: we need a panic mode to recover from Error
         assert len(matches) != 0, f"No Match!!! Rest text:\n{content[:20]}..."
+        
+        # if there has multiple match, select the longest one
         max_match_ind = -1
         max_length = 0
         for i, match in enumerate(matches):
@@ -44,6 +47,6 @@ def eat_token_by_token(text: str, regexes: [str], symbols: [str], exclude=None) 
         match = matches[max_match_ind]
 
         tokens.append(match)
-        ind += max_length
+        ind += max_length  # move ind to match next token
 
     return tokens
